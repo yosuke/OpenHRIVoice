29f57154070daabacb722d21b0d92cd29ced5ee2
Yosuke Matsusaka <yosuke.matsusaka@aist.go.jp>
2011-07-14 14:24:23 +0900

    inloop simulation examples

---
+++ examples/juliusrtc/ConsoleIn-de.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import sys
+import time
+import OpenRTM_aist
+import RTC
+ 
+consolein_spec = ["implementation_id", "ConsoleIn",
+                  "type_name",         "ConsoleIn",
+                  "description",       "Console input component",
+                  "version",           "1.0",
+                  "vendor",            "sample",
+                  "category",          "example",
+                  "activity_type",     "DataFlowComponent",
+                  "max_instance",      "10",
+                  "language",          "Python",
+                  "lang_type",         "script",
+                  ""]
+ 
+class ConsoleIn(OpenRTM_aist.DataFlowComponentBase):
+    def __init__(self, manager):
+        OpenRTM_aist.DataFlowComponentBase.__init__(self, manager)
+        self._data = RTC.TimedString(RTC.Time(0,0),"")
+        self._outport = OpenRTM_aist.OutPort("out", self._data)
+ 
+    def onInitialize(self):
+        self.registerOutPort("out", self._outport)
+        return RTC.RTC_OK
+ 
+    def onExecute(self, ec_id):
+        self._data.data = "guten morgen"
+        OpenRTM_aist.setTimestamp(self._data)
+        self._outport.write()
+        time.sleep(5)
+        self._data.data = "guten tag"
+        OpenRTM_aist.setTimestamp(self._data)
+        self._outport.write()
+        time.sleep(5)
+        return RTC.RTC_OK
+ 
+def MyModuleInit(manager):
+    profile = OpenRTM_aist.Properties(defaults_str=consolein_spec)
+    manager.registerFactory(profile,
+                            ConsoleIn,
+                            OpenRTM_aist.Delete)
+    comp = manager.createComponent("ConsoleIn")
+ 
+def main():
+    mgr = OpenRTM_aist.Manager.init(sys.argv)
+    mgr.setModuleInitProc(MyModuleInit)
+    mgr.activateManager()
+    mgr.runManager()
+ 
+if __name__ == "__main__":
+    main()
---
+++ examples/juliusrtc/run-en.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+echo "exiting existing components..."
+rtexit /localhost/`hostname`.host_cxt/JuliusRTC0.rtc
+rtexit /localhost/`hostname`.host_cxt/FestivalRTC0.rtc
+rtexit /localhost/`hostname`.host_cxt/ConsoleIn0.rtc
+rtexit /localhost/`hostname`.host_cxt/PulseAudioOutput0.rtc
+sleep 3
+
+echo "launching components..."
+gnome-terminal -x python ConsoleIn-en.py
+gnome-terminal -x festivalrtc
+gnome-terminal -x juliusrtc sample-en.grxml
+gnome-terminal -x pulseaudiooutput
+sleep 3
+
+echo "connecting components..."
+rtcon /localhost/`hostname`.host_cxt/ConsoleIn0.rtc:out /localhost/`hostname`.host_cxt/FestivalRTC0.rtc:text
+rtcon /localhost/`hostname`.host_cxt/FestivalRTC0.rtc:result /localhost/`hostname`.host_cxt/PulseAudioOutput0.rtc:AudioDataIn
+rtcon /localhost/`hostname`.host_cxt/PulseAudioOutput0.rtc:AudioDataOut /localhost/`hostname`.host_cxt/JuliusRTC0.rtc:data
+
+sleep 1
+
+echo "activating components..."
+rtact /localhost/`hostname`.host_cxt/JuliusRTC0.rtc
+rtact /localhost/`hostname`.host_cxt/FestivalRTC0.rtc
+rtact /localhost/`hostname`.host_cxt/ConsoleIn0.rtc
+rtact /localhost/`hostname`.host_cxt/PulseAudioOutput0.rtc
+
+echo "inloop simulation for 30 seconds..."
+sleep 30
+
+echo "existing components..."
+rtexit /localhost/`hostname`.host_cxt/JuliusRTC0.rtc
+rtexit /localhost/`hostname`.host_cxt/FestivalRTC0.rtc
+rtexit /localhost/`hostname`.host_cxt/ConsoleIn0.rtc
+rtexit /localhost/`hostname`.host_cxt/PulseAudioOutput0.rtc
