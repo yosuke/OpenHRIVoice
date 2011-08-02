968b9351b9740f3c3dbbdfef996500c11de1c947
Yosuke Matsusaka <yosuke.matsusaka@aist.go.jp>
2011-08-02 16:56:47 +0900

    forgot to commit change

--- openhrivoice/juliustographviz.py
+++ openhrivoice/juliustographviz.py
@@ -38,6 +38,33 @@ Examples:
   $ srgstojulius sample.grxml | juliustographviz | dot -Txlib
 '''
 
+def juliustographviz(lines):
+    ret = ''
+    fdfa = True
+    fsa = list()
+    dic = {}
+    for line in lines:
+        line = line.rstrip('\n')
+        if line == "DFAEND":
+            fdfa = False
+            continue
+        elif line == "DICEND":
+            break
+        if fdfa:
+            v = line.split(' ')
+            fsa.append((int(v[2]), int(v[1]), int(v[0])))
+        else:
+            v = line.split('\t')
+            dic[int(v[0])] = v[1].strip('[]')
+    ret += u"digraph finite_state_machine {\n"
+    ret += u"  rankdir=LR;\n"
+    for v in fsa:
+        if v[0] < 0:
+            break
+        ret += u'  S%i -> S%i [label = "%s"];\n' % (v[0], v[2], dic[v[1]])
+    ret += u"}\n"
+    return ret
+
 def main():
     encoding = locale.getpreferredencoding()
     sys.stdin = codecs.getreader(encoding)(sys.stdin, errors = "replace")
@@ -55,29 +82,7 @@ def main():
         print >>sys.stderr, 'OptionError:', e
         sys.exit(1)
 
-    fdfa = True
-    fsa = list()
-    dic = {}
-    for line in sys.stdin:
-        line = line.rstrip()
-        if line == "DFAEND":
-            fdfa = False
-            continue
-        elif line == "DICEND":
-            break
-        if fdfa:
-            v = line.split(' ')
-            fsa.append((int(v[2]), int(v[1]), int(v[0])))
-        else:
-            v = line.split('\t')
-            dic[int(v[0])] = v[1].strip('[]')
-    print u"digraph finite_state_machine {"
-    print u"  rankdir=LR;"
-    for v in fsa:
-        if v[0] < 0:
-            break
-        print u'  S%i -> S%i [label = "%s"];' % (v[0], v[2], dic[v[1]])
-    print u"}"
+    print juliustographviz(sys.stdin)
 
 if __name__ == '__main__':
     main()
